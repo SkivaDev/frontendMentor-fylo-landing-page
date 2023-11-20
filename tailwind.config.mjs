18498b348e33774b57590ed1e48535770b875919
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-11-19 22:20:01 -0500

    feat(project): :sparkles: add Proposition component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -16,12 +16,16 @@ export default {
 				"desaturated-blue": 'hsl(238, 22%, 44%)',
 				"bright-blue": 'hsl(224, 93%, 58%)',
 				"moderate-cyan": 'hsl(170, 45%, 43%)',
-				"light-grayish-blue": 'hsl(240, 75%, 98%)',
+				"light-grayish-blue": '#f8f8fe',
 				"light-gray": 'hsl(0, 0%, 75%)',
 			},
 			fontFamily: {
 				"Raleway": ['Raleway', 'sans-serif'],
 			},
+			borderColor: {
+				"moderate-cyan": 'hsl(170, 45%, 43%)',
+
+			}
 		},
 	},
 	plugins: [],
